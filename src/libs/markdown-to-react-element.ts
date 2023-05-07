b4645b4f881ba0b7732d65e6a9aa2f465fd24324
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-05-07 12:51:27 +0900

    feat: markdown to react-element

---
+++ src/libs/markdown-to-react-element.ts
@@ -0,0 +1,25 @@
+import CustomLink from "@/components/CustomLink"
+import { Fragment, createElement, ReactElement } from "react"
+import rehypeRaw from "rehype-raw"
+import rehypeReact from "rehype-react"
+import { remark } from "remark"
+import remarkBreaks from "remark-breaks"
+import remarkGfm from "remark-gfm"
+import remarkRehype from "remark-rehype"
+
+export const markdownToReactElement = (markdownContent: string): ReactElement => {
+  return remark()
+    .use(remarkGfm)
+    .use(remarkBreaks)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeReact, {
+      Fragment,
+      components: {
+        a: CustomLink,
+      },
+      createElement
+    })
+    .processSync(markdownContent).result
+}
+
